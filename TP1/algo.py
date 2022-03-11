5d72c2a8ca85099ae3f5f12a4f539f6297fbf45e
Natu17 <nlilienthal@itba.edu.ar>
2022-03-11 20:20:21 -0300

    dfslv

--- TP1/algo.py
+++ TP1/algo.py
@@ -35,3 +35,47 @@ def search(root, actions, condition, pick):
 
 dfs = lambda root,actions,condition: search(root,actions,condition, lambda F: F.pop())
 bfs = lambda root,actions,condition: search(root,actions,condition, lambda F: F.popleft())
+
+
+
+
+def dfslv(root, actions, condition,limit,step):
+    ex = dict()
+    ex[root] = 1
+    depth = 0
+    count = 0
+    F = deque([(Node(root),depth)])
+    while F:
+        if limit == count:
+            node = F.popleft()
+            if node[1] == limit:
+                limit += step
+                count = 0
+            else:
+                count = limit
+        else: 
+            node = F.pop()
+            if node[1] == limit:
+              aux = F.popleft()
+              if aux is not None:
+                if aux[1] == limit:
+                  limit += step
+                  count = 0
+                F.append(node)
+                node = aux    
+        state = node[0].state
+        
+        count = count + 1
+        for action in actions:
+            state = action.action(node[0].state)
+            if not(state in ex): #Create Node AFTER state not explored check
+                child = Node(state, node[0], action.actionName)
+                if condition(state):
+                    moves = ''
+                    while(child is not None):
+                        if(child.action is not None): moves = child.action + ' ' + moves
+                        child = child.parent
+                    return state
+                ex[state] = 1
+                F.append((child,count))
+    print('solution not found')
--- TP1/main.py
+++ TP1/main.py
@@ -5,12 +5,14 @@ import time
 
 #rubik.depth()
 #exit()
+n = 10
 
-while(True):
+while n:
+    n = n - 1
     scramble = rubik.scramble()
     print(scramble) 
     start = time.time()
-    solve = algo.dfs( scramble[0] , rubik.actions, rubik.check)
+    solve = algo.dfslv( scramble[0] , rubik.actions, rubik.check,3,2)
     end = time.time()
     print(solve)
     print(end - start)
