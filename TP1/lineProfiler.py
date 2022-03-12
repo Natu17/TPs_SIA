fd7e743bced1959b8c539a39b9118e3025c6a3e2
sespinaItba <sespina@itba.edu.ar>
2022-03-12 15:19:37 -0300

    remove extra stickers from prifilers

--- TP1/lineProfiler.py
+++ TP1/lineProfiler.py
@@ -5,13 +5,13 @@ profiler = line_profiler.LineProfiler()
 #profiler.add_function(algo.search)
 
 # wrapper = profiler(algo.bfs)
-# wrapper('owwobbbbyyooggggrrwwyrry' , rubik.actions, rubik.check)
+# wrapper('owwobbbbyyoogggrwwrry' , rubik.actions, rubik.check)
 
 # wrapper = profiler(algo.dfs)
-# wrapper('wwgrwrybobwogobgrogrybyy' , rubik.actions, rubik.check)
+# wrapper('wwgrwrybobwogobogrbyy' , rubik.actions, rubik.check)
 
 wrapper = profiler(algo.dfslv)
-wrapper('wgrgyowrwowrbgogryboybby' , rubik.actions, rubik.check,4,5)
+wrapper('wgrgyowrwowrbgoybobby' , rubik.actions, rubik.check,4,5)
 
 
 profiler.print_stats(open("line_profiler","w"))
--- TP1/profiler.py
+++ TP1/profiler.py
@@ -3,9 +3,9 @@ import cProfile, pstats ,algo,rubik
 
 profiler = cProfile.Profile()
 profiler.enable()
-algo.bfs( 'gwoyowbogrbgoyrgrbrwybyw' , rubik.actions, rubik.check)
-#algo.dfs( 'wgrgyowrwowrbgogryboybby' , rubik.actions, rubik.check)
-#algo.dfslv( 'wgrgyowrwowrbgogryboybby' , rubik.actions, rubik.check,4,5)
+algo.bfs( 'gwoyowbogrbgoyrbrwbyw' , rubik.actions, rubik.check)
+#algo.dfs( 'wgrgyowrwowrbgoybobby' , rubik.actions, rubik.check)
+#algo.dfslv( 'wgrgyowrwowrbgoybobby' , rubik.actions, rubik.check,4,5)
 profiler.disable()
 stats = pstats.Stats(profiler).strip_dirs().sort_stats('cumtime')
 stats.print_stats()
