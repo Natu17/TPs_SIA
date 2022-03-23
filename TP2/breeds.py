e5f29d5c251fcd7a5d3e3253369c5394003476f8
Natu17 <nlilienthal@itba.edu.ar>
2022-03-23 20:37:28 -0300

    multiple_breeds

--- TP2/breeds.py
+++ TP2/breeds.py
@@ -2,15 +2,29 @@ from audioop import reverse
 import random
 from sortedcontainers import SortedList
 
-def simpleBreeds(genotype1,genotype2):
+def simple_breeds(genotype1,genotype2):
     p = random.randint(0, len(genotype1))
     genotypeFinal = genotype1[0:p] + genotype2[p:len(genotype2)]
     return genotypeFinal
 
+def multiple_breeds(genotype1,genotype2,n):
+    if n < len(genotype1): 
+        points = SortedList()
+        l = [ i for i in range(1,len(genotype1))]
+        while len(points) < n:
+            p = random.choice(l)
+            l.remove(p)
+            points.add(p)
+        points.add(len(genotype1))
+        p1 = 0
+        genotypes = [[],[]]
+        while  len(points) > 0:
+            p2 = points.pop(0)
+            genotypes[0] += genotype1[p1:p2] 
+            genotypes[1] += genotype2[p1:p2]
+            aux = genotypes[0]
+            genotypes[0] = genotypes[1]
+            genotypes[1] = aux
+            p1 = p2
 
-
-
-
-
-
-
+    return genotypes
