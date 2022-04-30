5b82ee6e91b2db525ac1f496a93d6d10f4adf8f8
AgustinJerusalinsky <ajerusalinsky@itba.edu.ar>
2022-04-29 21:33:27 -0300

    Noise exercise
    Co-authored-by: Segundo Espina <sespina@itba.edu.ar>
    Co-authored-by: Natali Lilienthal <natalilienthal@itba.edu.ar>

--- TP3/utils.py
+++ TP3/utils.py
@@ -1,8 +1,15 @@
+import numpy as np
 import random
 
-random.seed(17)
 
-def noise(number):
+
+def noise(number, probability=0.2, seed=0):
+    if seed:
+        random.seed(seed)
+    to_ret = np.zeros(len(number))
     for i in range(0, len(number)):
-        if random.rand() < 0.2:
-            number[i] = 1-number[i]
+        if random.random() < probability:
+            to_ret[i] = 1-number[i]
+        else: 
+            to_ret[i] = number[i]
+    return to_ret
