b46387a112206a6f2bbee8aa76697f9768bd9b72
Natu17 <nlilienthal@itba.edu.ar>
2022-05-06 20:51:52 -0300

    cross_validation

--- TP3/metrics.py
+++ TP3/metrics.py
@@ -1,18 +1,28 @@
 import numpy
-import random
 from network import Network
+import copy
 
-def cross_validation(dataset, structure, activation, seed,k,epochs):
+def cross_validation(dataset, network_template, seed,k,epochs):
+    
+    if len(dataset) % k != 0:
+        raise ValueError("dataset must be divisible by k")
+
+    n = int(len(dataset)/k)
     start = 0
-    end = k
+    end = n
     test = dataset[start : end]
     training = dataset[end:]
-    while(end >= dataset.size()):
-        network = Network(structure, activation, seed)
+    
+    while(end <= len(dataset)):
+        network = copy.deepcopy(network_template)
         network.train(training, epochs)
         #metodo validacion test
+   
         start = end
-        end+=k
+        end+=n
         test = dataset[start : end]
         training = dataset[:start] + dataset[end : ]
-    
+       
+def metrics(dataset):
+        
+        
