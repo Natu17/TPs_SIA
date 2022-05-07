6d611bab264164d4337559d214ab52cc81119ce0
Natu17 <nlilienthal@itba.edu.ar>
2022-05-07 19:35:43 -0300

    metrics
    Co-authored-by: Segundo Espina <sespina@itba.edu.ar>
    Co-authored-by: Agustin jerusalinsky <ajerusalinsky@itba.edu.ar>

--- TP3/cross_validation.py
+++
@@ -1,2 +0,0 @@
-
-def cross_validation():
--- TP3/metrics.py
+++ TP3/metrics.py
@@ -1,28 +1,69 @@
-import numpy
+import numpy as np
 from network import Network
 import copy
 
-def cross_validation(dataset, network_template, seed,k,epochs):
-    
+def cross_validation(dataset, network_template,k,epochs,metric):
+
     if len(dataset) % k != 0:
         raise ValueError("dataset must be divisible by k")
 
     n = int(len(dataset)/k)
+    
+    groups = []
+    
+    # for i in range(k):
+    #     test = dataset[i*n:(i+1)*n]
+    #     train = dataset[:i*n] + dataset[(i+1)*n:]
+    #     groups.append((train,test))
+    
+    
     start = 0
     end = n
     test = dataset[start : end]
     training = dataset[end:]
-    
+    precision = (-1,-1)
+
+  
     while(end <= len(dataset)):
         network = copy.deepcopy(network_template)
-        network.train(training, epochs)
-        #metodo validacion test
-   
+        network.train(training, epochs = epochs)
+        p = metric(network,test)
+        if p >= precision[1]: precision = (network,p) 
         start = end
         end+=n
         test = dataset[start : end]
         training = dataset[:start] + dataset[end : ]
-       
-def metrics(dataset):
         
-        
+    return precision
+
+    
+def get_nearest_class(classes, value):
+    distances = [np.linalg.norm(value - c) for c in classes]
+    return distances.index(min(distances))
+
+def confusion_matrix(classes, predicted, expected):
+    matrix = np.zeros((len(classes),len(classes)))
+
+    for p,e in zip(predicted,expected):
+        class_idx = classes.index(e)
+        obtained_idx = get_nearest_class(classes,p)
+        matrix[class_idx][obtained_idx] += 1
+
+    accuracy = np.trace(matrix)/np.sum(matrix) 
+
+    return matrix, accuracy
+
+
+    
+
+def accuracy(network,dataset, epsilon = 0.1):
+    good = 0
+    bad = 0
+    for i in range(len(dataset)):
+        if (network.error([dataset[i]]) <= epsilon):
+            good+=1
+        else: bad+=1
+    p = good/(good + bad)
+    return p
+
+    
